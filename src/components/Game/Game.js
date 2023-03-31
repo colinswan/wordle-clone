36021fb1f01b9890c594d60a10d9c6c62274364c
Colin Swan <colinswan.cs@gmail.com>
2023-03-31 21:13:34 +0100

    updated tb

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -15,11 +15,7 @@ function Game() {
   const [guesses, setGuesses] = React.useState([]);
 
   function handleSubmitGuess(tentativeGuess) {
-    const nextGuess = {
-      value: tentativeGuess,
-      id: Math.random(),
-    };
-    setGuesses([...guesses, nextGuess]);
+    setGuesses([...guesses, tentativeGuess]);
   }
   return (
     <>
