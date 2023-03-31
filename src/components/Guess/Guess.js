4a70c2b7c78095f102543e3d357c5caebf546413
Colin Swan <colinswan.cs@gmail.com>
2023-03-31 21:29:46 +0100

    completed tutors version

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { range } from "../../utils";
+
+function Guess({ value }) {
+  return (
+    <p class="guess">
+      {range(5).map((num) => (
+        <span key={num} class="cell">
+          {value ? value[num] : undefined}
+        </span>
+      ))}
+    </p>
+  );
+}
+
+export default Guess;
