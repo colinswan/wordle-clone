fd459686d70ab8d75a618368f6ee12856bff9ed8
Colin Swan <colinswan.cs@gmail.com>
2023-04-01 15:06:28 +0100

    completed guess slots

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,14 +2,32 @@ import React from "react";
 
 import { range } from "../../utils";
 
-function Guess({ value }) {
+import { checkGuess } from "../../game-helpers";
+
+function Guess({ value, answer }) {
+  const GameResult = checkGuess(value, answer) || [];
+
+  console.log(GameResult);
+
   return (
-    <p class="guess">
-      {range(5).map((num) => (
-        <span key={num} class="cell">
-          {value ? value[num] : undefined}
-        </span>
-      ))}
+    <p className="guess">
+      {GameResult.map((result, index) => {
+        let className = "cell";
+
+        if (result.status === "correct") {
+          className = "cell correct";
+        } else if (result.status === "incorrect") {
+          className = "cell incorrect";
+        } else if (result.status === "misplaced") {
+          className = "cell misplaced";
+        }
+
+        return (
+          <span key={index} className={className}>
+            {value ? value[index] : undefined}
+          </span>
+        );
+      })}
     </p>
   );
 }
